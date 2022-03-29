d4ba439ecc617e2e9febfdd969cbd546029b4052
mohamedahmedgomaa <mido.15897@gmail.com>
2022-03-29 16:07:27 -0700

    some init msg

---
+++ app/Http/Livewire/FormBook.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FormBook extends Component
+{
+    public function render()
+    {
+        return view('livewire.form-book');
+    }
+}
